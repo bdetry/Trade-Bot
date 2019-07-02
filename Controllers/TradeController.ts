a6c286d1c859aba7f5c8a3bf1273cd284fa9cb9b
Boris Detry <detryboris@yahoo.com>
2019-07-02 09:36:44 +0200

    comments

--- Controllers/TradeController.ts
+++ Controllers/TradeController.ts
@@ -1,11 +1,20 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { GlobalString } from '../globals';
 
+/**
+ * Class that trade cryptos
+ * */
 class TradeController {
     constructor() {
         this.doTrade = this.doTrade.bind(this);
     }
 
+    /**
+     * Called on http get /trade - execute actions and place order
+     * @param req
+     * @param res
+     * @param next
+     */
     doTrade(req: Request, res: Response, next: NextFunction) {
 
         let secuKey = req.query.key;
@@ -14,11 +23,17 @@ class TradeController {
 
         if (secuKey == key) {
 
+            // TO DO
+            // Get stored prices
+            // Get market prices
+            // Create arder from rule
+            // Make order
+            // Save localy
+            // Load to slack
+
         } else {
             res.status(401).json({message: "Wrong key"})
         }
-
-        console.log(req.query.key);
     }
 
 }
