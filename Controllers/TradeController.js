e54bfa7d9f8bed2c96876543e9aca05c3a7710be
Boris Detry <detryboris@yahoo.com>
2019-07-05 01:54:28 +0200

    Revert "realise des achats"
    This reverts commit fb3d8e8df59e2f1e9a4551ade48bc0f0a4eb4983.

--- Controllers/TradeController.js
+++ Controllers/TradeController.js
@@ -1,73 +1,18 @@
 "use strict";
 const globals_1 = require("../globals");
-const DataGetterClass = require("../DataGetter/DataGetterClass");
-const StrategiesClass = require("./../Strategies/StrategiesClass");
-const DataSaverClass = require("../DataSaver/DataSaverClass");
-/**
- * Class that trade cryptos
- * */
 class TradeController {
     constructor() {
         this.doTrade = this.doTrade.bind(this);
-        this.dataGetter = new DataGetterClass();
-        this.dataSaver = new DataSaverClass();
     }
-    /**
-     * Called on http get /trade - execute actions and place order
-     * @param req
-     * @param res
-     * @param next
-     */
     doTrade(req, res, next) {
         let secuKey = req.query.key;
         let key = globals_1.GlobalString.TRADESECURITYKEY;
         if (secuKey == key) {
-            //Get local prices
-            return this.dataGetter.GetLocalPrice()
-                .then(x => {
-                //Last buy price
-                let lastSavedPrice = x[0].asks[0][0];
-                return this.dataGetter.GetLocalAccounts()
-                    .then(r => {
-                    //Get account balances
-                    let moneyZeroAccount;
-                    let moneyOneAccount;
-                    if (r[0].currency == "EUR") {
-                        moneyZeroAccount = r[0];
-                        moneyOneAccount = r[1];
-                    }
-                    else if (r[0].currency == "LTC") {
-                        moneyZeroAccount = r[1];
-                        moneyOneAccount = r[0];
-                    }
-                    //Get currrent price
-                    return this.dataGetter.GetCurrencyPrice("LTC-EUR")
-                        .then(x => {
-                        //current price
-                        let currentPrice = x.data.asks[0][0];
-                        //Choise maker
-                        let strategies = new StrategiesClass(+lastSavedPrice, +currentPrice, +moneyZeroAccount.available, +moneyOneAccount.available);
-                        strategies.ApplyStrategieAndCreateOrder("strat1");
-                        return this.dataSaver.PlaceOrder(strategies.orders[0])
-                            .then(ordered => {
-                            console.log(ordered);
-                        }).catch(e => {
-                            throw new Error(e);
-                        });
-                    }).catch(e => {
-                        throw new Error(e);
-                    });
-                }).catch(e => {
-                    throw new Error(e);
-                });
-            }).catch(e => {
-                new Error(e);
-                res.status(500).json({ stack: e.stack });
-            });
         }
         else {
             res.status(401).json({ message: "Wrong key" });
         }
+        console.log(req.query.key);
     }
 }
 module.exports = TradeController;
